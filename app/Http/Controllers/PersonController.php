5a4d2a8191eb1f18cd5824cad943da4978b29877
GeorgeKarpenko <mynameisgeorgekarpenko@yandex.ru>
2020-10-23 22:59:16 +0300

    master

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -7,15 +7,9 @@ use Illuminate\Http\Request;
 
 class PersonController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index(Person $people, $group_id)
     {
         $people = $people->activePersonsInANonArchiveGroup($group_id);
         return response()->json($people); 
     }
-
 }
